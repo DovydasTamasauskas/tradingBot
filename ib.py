6a923ab05ec7cb00013e39fdcd35cac516545e38
Dovydas Tamasauskas <dovydastamasauskas@Dovydass-MacBook-Pro.local>
2023-03-06 16:42:07 +0200

    init

---
+++ ib.py
@@ -0,0 +1,61 @@
+from ib_insync import *
+
+
+def getCandlesLow(array):
+    return min(list(map(lambda x: x.low, array)))
+
+
+def getCandlesHigh(array):
+    return max(list(map(lambda x: x.high, array)))
+
+
+def getAskPrice():
+    market = ib.reqMktData(contract, '', False, False)
+    ib.sleep(2)
+    return market.ask
+
+
+def getHistoricalData():
+    return ib.reqHistoricalData(
+        contract, endDateTime='', durationStr=HISTORY_DATA_INTERVAL,
+        barSizeSetting=TIME_INTERVAL, whatToShow='MIDPOINT', useRTH=True)
+
+
+def printPosition(stopLoss, takeProfit, entry, positionType):
+    if MAX_STOP_LOSS > abs(stopLoss - entry):
+        print(positionType)
+        print("entry =      "+str(entry))
+        print("stopLoss =   "+str(stopLoss))
+        print("takeProfit = "+str(takeProfit))
+    else:
+        print('Position exceded stop loss limit')
+
+
+# ----------------------------
+POSITION_TYPE = "LONG"
+PAIR = 'EURUSD'
+STOP_LOSS_CANDLE_COUNT = 3
+MAX_STOP_LOSS = 0.02
+TIME_INTERVAL = "15 mins"
+HISTORY_DATA_INTERVAL = "1 D"
+# add to stopLoss??
+# ----------------------------
+
+ib = IB()
+ib.connect('127.0.0.1', 7497, clientId=1)
+contract = Forex(PAIR)
+
+bars = getHistoricalData()
+marketPrice = getAskPrice()
+
+if POSITION_TYPE == "LONG":
+    stopLoss = getCandlesLow(bars[-STOP_LOSS_CANDLE_COUNT:])
+    takeProfit = round((marketPrice - stopLoss)*1.5+marketPrice, 5)
+
+    printPosition(stopLoss, takeProfit, marketPrice, POSITION_TYPE)
+
+if POSITION_TYPE == "SHORT":
+    stopLoss = getCandlesHigh(bars[-STOP_LOSS_CANDLE_COUNT:])
+    takeProfit = round(marketPrice-(stopLoss-marketPrice)*1.5, 5)
+
+    printPosition(stopLoss, takeProfit, marketPrice, POSITION_TYPE)
---
+++ main.py
@@ -0,0 +1,35 @@
+import imaplib
+import email
+import time
+
+
+def sleep(sleepTime):
+    for x in range(sleepTime):
+        time.sleep(1)
+        print(sleepTime-x, end="\r")
+
+
+imap_server = "imap.gmail.com"
+email_address = "jonas.bartusevicius1990@gmail.com"
+password = "ylobdthrnqfrujmn"
+
+while True == True:
+    imap = imaplib.IMAP4_SSL(imap_server)
+    imap.login(email_address, password)
+    imap.select("Inbox")
+    _, msgs = imap.search(None, "(UNSEEN)")
+    print("getting messages")
+
+    for msg in msgs[0].split():
+        _, data = imap.fetch(msg, "(RFC822)")
+        message = email.message_from_bytes(data[0][1])
+        subject = message.get("Subject")
+        print(subject)
+        splited = subject.split()
+        print(splited)
+        requirements = ["stopLoss", "maxStopLoss"]
+        if splited[0].find(requirements[0]):
+            if splited[0].find("="):
+                print(splited[0].split("=")[1])
+    sleep(5)
+    imap.close()
