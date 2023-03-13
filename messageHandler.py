c0f56c02a061e956aeff90ba613f2c7afa684906
Dovydas Tamasauskas <dovydastamasauskas@Dovydass-MacBook-Pro.local>
2023-03-13 11:29:59 +0200

    clean up

--- messageHandler.py
+++ messageHandler.py
@@ -1,17 +1,8 @@
 import notification.notify as notify
 import broker.interactiveBrokers as interactiveBrokers
 import broker.getters as getters
-import broker.defaultProps as defaultProps
 import shared.messages as messages
-import shared.functions as functions
-
-
-def getCandlesLow(array):
-    return min(list(map(lambda x: x.low, array)))
-
-
-def getCandlesHigh(array):
-    return max(list(map(lambda x: x.high, array)))
+import riskManagment
 
 
 def sendMessage(connection, stopLoss, takeProfit, entry, positionType, pair, maxStopLoss):
@@ -48,24 +39,11 @@ def main(connection, p):
     pair = getters.getPair(p)
     contract = interactiveBrokers.setContract(pair)
 
-    historicalData = interactiveBrokers.getHistoricalData(
-        ib, contract, getters.getTime(p), getters.getHistoryDataInterval(p))
     marketPrice = interactiveBrokers.getAskPrice(ib, contract)
 
-    if historicalData != None and marketPrice != None:
-        stopLossCanldes = getters.getStopLossCanldes(p)
-        takeProfitRatio = getters.getTakeProfitRatio(p)
-        stopLossCandles = functions.slice(historicalData, -stopLossCanldes)
+    position = getters.getPosition(p)
+    stopLoss = riskManagment.getStopLoss(ib, contract, p)
+    takeProfit = riskManagment.getTakeProfit(ib, contract, stopLoss, p)
 
-        position = getters.getPosition(p)
-        if getters.isLong(position):
-            stopLoss = getCandlesLow(stopLossCandles)
-            takeProfit = (marketPrice-stopLoss) * takeProfitRatio + marketPrice
-
-        if getters.isShort(position):
-            stopLoss = getCandlesHigh(stopLossCandles)
-            takeProfit = marketPrice-(stopLoss-marketPrice) * takeProfitRatio
-
-        takeProfit = round(takeProfit, 5)
-        sendMessage(connection, stopLoss, takeProfit,
-                    marketPrice, position, pair, getters.getMaxStopLoss(p))
+    sendMessage(connection, stopLoss, takeProfit,
+                marketPrice, position, pair, getters.getMaxStopLoss(p))
--- riskManagment.py
+++ riskManagment.py
@@ -5,21 +5,33 @@ import broker.getters as getters
 
 
 def getStopLoss(ib, contract, params):
-
     stopLossCanldes = getters.getStopLossCanldes(params)
     time = getters.getTime(params)
     historicalDataInterval = getters.getHistoryDataInterval(params)
+    position = getters.getPosition(params)
 
     historicalData = interactiveBrokers.getHistoricalData(
         ib, contract, time, historicalDataInterval)
     stopLossCandles = functions.slice(historicalData, -stopLossCanldes)
-    stopLoss = getCandlesLow(stopLossCandles)
-    print(stopLoss)
+
+    if getters.isLong(position):
+        stopLoss = getCandlesLow(stopLossCandles)
+    if getters.isShort(position):
+        stopLoss = getCandlesHigh(stopLossCandles)
     return stopLoss
 
 
-def getTakeProfit():
-    return 0.06
+def getTakeProfit(ib, contract, stopLoss, params):
+    takeProfitRatio = getters.getTakeProfitRatio(params)
+    position = getters.getPosition(params)
+
+    marketPrice = interactiveBrokers.getAskPrice(ib, contract)
+
+    if getters.isLong(position):
+        takeProfit = (marketPrice-stopLoss) * takeProfitRatio + marketPrice
+    if getters.isShort(position):
+        takeProfit = marketPrice-(stopLoss-marketPrice) * takeProfitRatio
+    return round(takeProfit, 5)
 
 
 def getCandlesLow(array):
