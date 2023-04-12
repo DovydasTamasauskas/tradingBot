a1712fb6002954c94cc7c776994f331f39078376
Dovydas Tamasauskas <dovydastamasauskas@Dovydass-MacBook-Pro.local>
2023-04-12 12:24:42 +0300

    itests fix

--- iTest/riskManagmentITest.py
+++ iTest/riskManagmentITest.py
@@ -7,7 +7,17 @@ import iTest.heplers as heplers
 
 def stopLossITest():
     heplers.testTitle("stopLoss should be calculated correctly")
-    resultJSON = interactiveBrokers.handlePosition(heplers.createJson())
+    resultJSON = interactiveBrokers.handlePosition({
+        "position": "long",
+        "pair": "EURUSD",
+        "size": 100,
+        "time": "15 mins",
+        "stopLossPercent": 2,
+        "maxStopLoss": 2,
+        "limitPrice": 100,
+        "sendResultEmail": False,
+        "logEnteredPosition": False
+    })
 
     stopLoss = getters.getStopLoss(resultJSON)
     if stopLoss == 98:
@@ -18,7 +28,17 @@ def stopLossITest():
 
 def takeProfitITest():
     heplers.testTitle("takeProfit should be calculated correctly")
-    resultJSON = interactiveBrokers.handlePosition(heplers.createJson())
+    resultJSON = interactiveBrokers.handlePosition({
+        "position": "long",
+        "pair": "EURUSD",
+        "size": 100,
+        "time": "15 mins",
+        "stopLossPercent": 2,
+        "maxStopLoss": 2,
+        "limitPrice": 100,
+        "sendResultEmail": False,
+        "logEnteredPosition": False
+    })
 
     takeProfit = getters.getTakeProfit(resultJSON)
     if takeProfit == 103:
@@ -27,20 +47,6 @@ def takeProfitITest():
         log.error("test failed. Got:" + str(takeProfit) + " Expected: 103")
 
 
-def takeProfitStopLossITest():
-    heplers.testTitle("TakeProfit and stopLoss should be calculated correctly")
-    resultJSON = interactiveBrokers.handlePosition(heplers.createJson())
-
-    stopLoss = getters.getStopLoss(resultJSON)
-    takeProfit = getters.getTakeProfit(resultJSON)
-    if stopLoss == 98 and takeProfit == 103:
-        log.success("test passed")
-    else:
-        log.error("test failed. Got:" + str(takeProfit) +
-                  ", " + str(stopLoss) + " Expected: 103, 98")
-
-
 def runTests():
     stopLossITest()
     takeProfitITest()
-    takeProfitStopLossITest()
